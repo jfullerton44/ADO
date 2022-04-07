df3a5141c9d7ae3032a3e0076d9cba567a6beb61
Jake Fullerton <jfullerton@microsoft.com>
2022-04-07 10:14:03 -0700

    all the files

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # ADO
+
+
+## Usage
+
+Call link.go with the two commit urls as command line arguments, the older commit url goes first
+
+```
+go run link.go https://dev.azure.com/yourOrg/yourProject/_git/repoName/commit/25dc190b6fbe683976578abdfe7936f22607e/refName=refs/heads/main https://dev.azure.com/yourOrg/yourProject/_git/repoName/commit/1932847a190b6fbe683976578abdfe7936f22607e/refName=refs/heads/main
+
+go run link.go OLD_COMMIT_URL NEW_COMMIT_URL
+
+```
---
+++ link.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	if len(os.Args) != 3 {
+		return
+	}
+	out, err := getDiffLink(os.Args[1], os.Args[2])
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	fmt.Println(out)
+}
+
+// Takes in two url links and returns a url with the diff between the 2 links
+func getDiffLink(url1 string, url2 string) (string, error) {
+	url1 = strings.ReplaceAll(url1, "?", "/")
+	url2 = strings.ReplaceAll(url2, "?", "/")
+	location1 := -1
+	arr1 := strings.Split(url1, "/")
+	for i, v := range arr1 {
+		if v == "commit" {
+			location1 = i + 1
+		}
+	}
+	if location1 == -1 {
+		return "", fmt.Errorf("url1 bad url")
+	}
+	location2 := -1
+	arr2 := strings.Split(url2, "/")
+	for i, v := range arr2 {
+		if v == "commit" {
+			location2 = i + 1
+		}
+	}
+	if location2 == -1 {
+		return "", fmt.Errorf("url2 bad url")
+	}
+
+	return fmt.Sprintf("https://dev.azure.com/devdiv/OnlineServices/_git/tunnels/branchCompare?baseVersion=GC%s&targetVersion=GC%s", arr1[location1], arr2[location2]), nil
+}
